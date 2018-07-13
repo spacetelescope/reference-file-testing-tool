fe25a6373604dbb2c234b11a754665cac10ccfdf
Mees Fix <mfix@stsci.edu>
2018-07-13 11:18:56 -0500

    Added functionality to ingest all JWST data. Uses dask for multiprocessing. Commiting reftest.py as well

--- reference_file_testing_tool/db.py
+++ reference_file_testing_tool/db.py
@@ -2,7 +2,7 @@
 
 Usage:
   db_utils (create | remove) <db_path>
-  db_utils (add | replace | force) <db_path> <file_path>
+  db_utils (add | replace | force | full_reg_set) <db_path> <file_path>
 
 Arguments:
   <db_path>     Absolute path to database. 
@@ -17,7 +17,11 @@ import glob
 import os
 
 from astropy.io import fits
+from dask import compute, delayed
+from dask.diagnostics import ProgressBar
 from docopt import docopt
+import itertools
+import re
 from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -48,7 +52,6 @@ class TestData(Base):
 
 
     def __init__(self, filename):
-        # Populates row when instance of class is envoked.
         header = fits.getheader(filename)
         path, name = os.path.split(filename)
         
@@ -70,7 +73,125 @@ class TestData(Base):
         self.SUBSTRT2 = header.get('SUBSTRT2')
         self.SUBSIZE1 = header.get('SUBSIZE1')
         self.SUBSIZE2 = header.get('SUBSIZE2')
+    
+def build_dask_delayed_list(function, data):
+    """Build list of dask delayed objects for functions with single arguments.
+    May want to expand for more arguments in the future.
 
+    Parameters
+    ----------
+    function: func
+        Function to multiprocess
+    data: list-like
+        List of data passed to function.
+    
+    Returns
+    -------
+    dask_delayed_list: list
+        List of dask delayed objects to run in parallel.
+    """
+    
+    dask_delayed_list = []
+
+    for item in data:
+        dask_delayed_list.append(delayed(function)(item))
+
+    return dask_delayed_list
+
+
+def walk_filesystem(data_dir):
+    """Wrapper for os.walk to return files in directorys
+    below root.
+    
+    Parameters
+    ----------
+    data_dir: str
+        Directory to look for files in.
+    
+    Returns
+    -------
+    full_path: list
+        List absolute paths to files in side of data_dir
+    """
+    # We only want uncalibrabrated products.
+    filetypes = ['uncal.fits',
+                 'rate.fits',
+                 'rateints.fits',
+                 'trapsfilled.fits',
+                 'dark.fits']
+
+    for root, dirs, files in os.walk(data_dir):
+        full_paths = [os.path.join(root, filename) 
+                      for filename in files 
+                      if any(
+                             filetype in filename 
+                             for filetype in filetypes
+                             )
+                     ]
+    
+    return full_paths
+
+def find_all_datasets(top_dir):
+    """Crawl through the JWST test regression datasystem
+    to locate files.
+
+    Parameters
+    ----------
+    top_dir: str
+        top level dir to crawl down from.
+    
+    Returns
+    -------
+    final_paths: list
+        A list of all the files that were located.
+    """
+    
+    top_levels = []
+    
+    for item in os.listdir(top_dir):
+        full_path = os.path.join(top_dir, item)
+        pattern = re.compile('[a-z]{2}\d{5}')
+        if pattern.match(item) is not None:
+            top_levels.append(full_path)
+    
+    results = build_dask_delayed_list(walk_filesystem, top_levels[:2])
+    
+    with ProgressBar():
+        final_paths = list(itertools.chain(*compute(results)[0]))
+
+    return final_paths
+
+def bulk_populate(file_path, db_path):
+    """Populate database with in parallel.
+
+    Parameters
+    ----------
+    file_path: str
+        Data location
+    db_path: str
+        Absolute pat to database
+    
+    Returns
+    -------
+    None
+    """
+    
+    print("GATHERING DATA, THIS CAN TAKE A FEW MINUTES....")
+    final_paths = find_all_datasets(file_path)
+    
+    data = build_dask_delayed_list(TestData, final_paths)
+    
+    print("EXTRACTING KEYWORDS....")
+    with ProgressBar():
+        data_to_insert = compute(data)[0]
+    
+    data_to_ingest = []
+    for dataset in data_to_insert:
+        data_to_ingest.append(delayed(commit_session)(dataset, db_path))
+        
+    print("INSERTING INTO DB....")
+    with ProgressBar():
+        compute(data_to_ingest)
 
 def data_exists(fname, session):
     """
@@ -123,15 +244,28 @@ def load_session(db_path=None):
     session: sqlalchemy.orm.Session
     """
 
-    # set up a session with the database
     if db_path is None:
         print("db_path = None, SUPPLY ABSOLUTE PATH TO DB!")
     else:
         engine = create_engine('sqlite:///{}'.format(db_path), echo=False)
         Session = sessionmaker(bind=engine)
         session = Session()
-        print("Connected to DB at {}".format(db_path))
         return session
+
+def commit_session(data, db_path):
+    """Load and commit additions to DB
+
+    Parameters
+    ----------
+    data: object
+        Instance of TestData
+    db_path: str
+        Location of database
+    """
+    
+    session = load_session(db_path)
+    session.add(data)
+    session.commit()
 
 
 def create_test_data_db(db_path):
@@ -144,9 +278,11 @@ def create_test_data_db(db_path):
         Absolute path to save the DB
     """
 
-    engine = create_engine('sqlite:///{}'.format(db_path), echo=False)
-    Base.metadata.create_all(engine)
-    print("CREATED DB: {}".format(db_path))
+    if os.path.exists(db_path):
+        print("{} EXISTS ALREADY!".format(db_path))
+    else:
+        engine = create_engine('sqlite:///{}'.format(db_path), echo=False)
+        Base.metadata.create_all(engine)
 
 
 def add_test_data(file_path, db_path=None, force=False, replace=False):
@@ -156,7 +292,7 @@ def add_test_data(file_path, db_path=None, force=False, replace=False):
     Parameters
     ----------
     file_path: str
-        Absolute path for data file to add
+        Absolute path for data file to add or data location
     db_path: str
         Absolute path to database on local machince
     force: bool
@@ -169,26 +305,35 @@ def add_test_data(file_path, db_path=None, force=False, replace=False):
     None
     """
 
+    # Create DB session
     session = load_session(db_path)
+
+    # For files in the path provided
     for fname in glob.glob(file_path):
+        # Check if file exists in database
         query_result = data_exists(fname, session)
         if query_result.count() != 0 and not (force or replace):
-            print("There is already test data with the same parameters. To add the data anyway use 'db_utils force <db_path> <file_path>'")
+            # If file exists and you don't want to force add or replace
+            # let the user know this file is in the database and how
+            # to add by force.
+            print("There is already test data with the same parameters. \
+                   To add the data anyway use \
+                   db_utils force <db_path> <file_path>")
         elif query_result and replace:
             session.delete(query_result.first())
             session.add(TestData(fname))
             session.commit()
-            print("Replaced {} with {}".format(query_result.first().filename,
+            print("REPLACED {} WITH {}".format(query_result.first().filename,
                                                file_path))
         else:
             new_test_data = TestData(fname)
             session.add(new_test_data)
             session.commit()
-            print("Added {} to database".format(file_path))
+            print("ADDED {} TO DATABASE".format(file_path))
 
 
 def main():
-    """Main to parse command line entries.
+    """Main to parse command line arguments.
 
     Parameters
     ----------
@@ -210,11 +355,8 @@ def main():
                       db_path=args['<db_path>'], 
                       force=args['force'], 
                       replace=args['replace'])
+    elif args['full_reg_set']:
+        bulk_populate(args['<file_path>'],
+                      args['<db_path>'])
     else:
-        # Maybe try to open a session with file and if it fails, don't delete
-        # Else, delete.
-        if args['<db_path>'].endswith('.db'):
-            os.remove(args['<db_path>'])
-            print("DELETED DB {}".format(args['<db_path>']))
-        else:
-            print('TO DELETE DB, FILE NEEDS TO END WITH ".db"')
+        print("Needs some work....")
---
+++ reference_file_testing_tool/reftest.py
@@ -0,0 +1,173 @@
+from __future__ import print_function
+from . import db
+
+import os
+from jwst.pipeline import calwebb_dark, calwebb_image2, calwebb_spec2
+
+try:
+    from jwst.pipeline import SloperPipeline as Detector1Pipeline
+except ImportError:
+    from jwst.pipeline import Detector1Pipeline
+
+from jwst import datamodels
+import crds
+from astropy.io import fits
+import logging
+from sqlalchemy import or_
+try:
+    from cStringIO import StringIO      # Python 2
+except ImportError:
+    from io import StringIO
+
+p_mapping = {
+    "META.EXPOSURE.TYPE": "META.EXPOSURE.P_EXPTYPE",
+    "META.INSTRUMENT.BAND": "META.INSTRUMENT.P_BAND",
+    "META.INSTRUMENT.DETECTOR": "META.INSTRUMENT.P_DETECTOR",
+    "META.INSTRUMENT.CHANNEL": "META.INSTRUMENT.P_CHANNEL",
+    "META.INSTRUMENT.FILTER": "META.INSTRUMENT.P_FILTER",
+    "META.INSTRUMENT.PUPIL": "META.INSTRUMENT.P_PUPIL",
+    "META.INSTRUMENT.MODULE": "META.INSTRUMENT.P_MODULE",
+    "META.SUBARRAY.NAME": "META.SUBARRAY.P_SUBARRAY",
+    "META.INSTRUMENT.GRATING": "META.INSTRUMENT.P_GRATING",
+    "META.EXPOSURE.READPATT": "META.EXPOSURE.P_READPATT"
+}
+
+meta_to_fits = {
+    'META.INSTRUMENT.NAME': 'INSTRUME',
+    'META.EXPOSURE.READPATT': 'READPATT',
+    'META.EXPOSURE.TYPE': 'EXP_TYPE',
+    'META.INSTRUMENT.BAND': 'BAND',
+    'META.INSTRUMENT.CHANNEL': 'CHANNEL',
+    'META.INSTRUMENT.DETECTOR': 'DETECTOR',
+    'META.INSTRUMENT.FILTER': 'FILTER',
+    'META.INSTRUMENT.GRATING': 'GRATING',
+    'META.SUBARRAY.NAME': 'SUBARRAY'
+}
+
+IMAGING = ['fgs_image', 'fgs_focus', 'fgs_skyflat', 'fgs_intflat', 'mir_image',
+           'mir_tacq', 'mir_lyot', 'mir_4qpm', 'mir_coroncal', 'nrc_image',
+           'nrc_tacq', 'nrc_coron', 'nrc_taconfirm', 'nrc_focus', 'nrc_tsimage',
+           'nis_image', 'nis_dark', 'nis_ami', 'nis_tacq', 'nis_taconfirm', 'nis_focus',
+           'nrs_tacq', 'nrs_taslit', 'nrs_taconfirm', 'nrs_confirm', 'nrs_image',
+           'nrs_focus', 'nrs_mimf', 'nrs_bota']
+
+def get_pipelines(exp_type):
+    if 'DARK' in exp_type:
+        return [calwebb_dark.DarkPipeline()]
+    elif 'FLAT' in exp_type:
+        return [Detector1Pipeline()]
+    elif exp_type.lower() in IMAGING:
+        return [Detector1Pipeline(), calwebb_image2.Image2Pipeline()]
+    else:
+        return [Detector1Pipeline(), calwebb_spec2.Spec2Pipeline()]
+
+def override_reference_file(ref_file, pipeline):
+    dm = datamodels.open(ref_file)
+    for step in pipeline.step_defs.keys():
+        # check if a step has an override_<reftype> option
+        if hasattr(getattr(pipeline, step), 'override_{}'.format(dm.meta.reftype)):
+            setattr(getattr(pipeline, step), 'override_{}'.format(dm.meta.reftype), ref_file)
+            print('Setting {} in {} step'.format('override_{}'.format(dm.meta.reftype), step))
+
+    return pipeline
+
+def test_reference_file(ref_file, data_file):
+    """
+    Override CRDS reference file with the supplied reference file and run
+    pipeline with supplied data file.
+    
+    Parameters
+    ----------
+    ref_file: str
+        Path to reference file.
+    data_file: str
+        Path to data file.
+    """
+
+    # redirect pipeline log from sys.stderr to a string
+    log_stream = StringIO()
+    stpipe_log = logging.Logger.manager.loggerDict['stpipe']
+    stpipe_log.handlers[0].stream = log_stream
+
+    # allow invalid keyword values
+    os.environ['PASS_INVALID_VALUES'] = '1'
+
+    print('Testing {}'.format(data_file))
+    result = data_file
+    try:
+        for pipeline in get_pipelines(fits.getheader(data_file)['EXP_TYPE']):
+            pipeline = override_reference_file(ref_file, pipeline)
+            print('Running {}'.format(pipeline))
+            result = pipeline.run(result)
+            print('Completed {}'.format(pipeline))
+
+        return 1
+
+    except Exception as err:
+        print('{} failed with error {}'.format(data_file, (err.message)))
+        return 0
+
+
+def find_matches(ref_file, session, max_matches=None):
+    """
+    
+    Parameters
+    ----------
+    ref_file: str
+        File path to reference file to test
+    max_matches: int
+        Maximum matches to return. (Default=-1, return all matches)
+
+    Returns
+    -------
+    matches: list
+        a list of filenames
+
+    """
+    dm = datamodels.open(ref_file)
+    context = crds.heavy_client.get_processing_mode('jwst')[1]
+    pmap = crds.rmap.load_mapping(context)
+    imap = pmap.get_imap(dm.meta.instrument.name)
+    rmap = imap.get_rmap(dm.meta.reftype)
+    meta_attrs = rmap.get_required_parkeys()
+    meta_attrs.remove('META.OBSERVATION.DATE')
+    meta_attrs.remove('META.OBSERVATION.TIME')
+
+    query_args = []
+    keys_used = []
+    for attr in meta_attrs:
+
+        # Deal with OR values
+        if p_mapping[attr].lower() in dm.to_flat_dict():
+            p_attr = p_mapping[attr]
+
+            if '|' in dm[p_attr.lower()]:
+
+                or_vals = dm[p_attr.lower()].split('|')[:-1]
+                or_vals = [val.strip() for val in or_vals]
+                query_args.append(or_(getattr(db.TestData, meta_to_fits[attr]) == val for val in or_vals))
+                keys_used.append([meta_to_fits[attr], dm[p_attr.lower()]])
+
+        # Ignore special CRDS-only values
+        elif dm[attr.lower()] in ['GENERIC', 'N/A', 'ANY']:
+            pass
+
+        # Normal values
+        else:
+            query_args.append(getattr(db.TestData, meta_to_fits[attr]) == dm[attr.lower()])
+            keys_used.append([meta_to_fits[attr], dm[attr.lower()]])
+
+    query_string = '\n'.join(['\t{} = {}'.format(key[0], key[1]) for key in keys_used])
+    print('Searching DB for test data with\n'+query_string)
+    query_result = session.query(db.TestData).filter(*query_args)
+    filenames = [result.filename for result in query_result]
+    print('Found {} instances:'.format(len(filenames)), end="")
+    print('\n'+'\n'.join(['\t'+f for f in filenames]))
+    if filenames:
+        if max_matches > 0:
+            print('Using first {} matches'.format(max_matches))
+    else:
+        print('\tNo matches found')
+
+    return filenames[:max_matches]
+
